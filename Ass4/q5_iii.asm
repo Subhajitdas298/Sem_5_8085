ba7cde69cfd41164db6f6ec114ce66596ec8ee33
Subhajitdas298 <subhajitdas298@live.com>
2017-11-08 10:57:16 +0530

    Assignment 4 question 5 complete

---
+++ Ass4/q5_iii.asm
@@ -0,0 +1,30 @@
+# ORG 2000
+	   LXI H,2100
+	   INX H
+	   MVI M,00
+	   MVI C,00
+	   MVI A,99
+	   SBI 90
+	   INR A
+	   DAA
+	   MVI B,30
+	   ADD B
+	   JNC SKIP1
+	   MVI C,01
+
+SKIP1:	   DAA
+	   JNC SKIP2
+	   MVI C,01
+
+SKIP2:	   DCR C
+	   JZ STORE
+	   MOV E,A
+	   MVI A,99
+	   SUB E
+	   INR A
+	   DAA
+	   MVI M,01
+
+STORE:	   DCX H
+	   MOV M,A
+	   HLT
